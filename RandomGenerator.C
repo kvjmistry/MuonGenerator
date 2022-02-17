8658a795cc6080af8f0d707c242ac6a0f43a457a
Krishan Mistry <kvjmistry@gmail.com>
2022-02-17 11:13:50 -0600

    fix descrepancies with standard nexus file and mceq simulation

--- RandomGenerator.C
+++ RandomGenerator.C
@@ -12,36 +12,65 @@ void GenerateRandom(std::vector<double> weights, std::vector<double> azimuth, st
     std::discrete_distribution<int> dist(std::begin(weights), std::end(weights));
     std::default_random_engine gen;  // Generator
     gen.seed(time(0)); // Seed with the current time for uniqueness
-    int N_samp = 10000000;
+    int N_samp = 1e6;
     std::vector<int> samples(N_samp);
+
+
+    std::vector<double> zeni_edges= {0.        , 0.20033484, 0.28379411, 0.34816602, 0.40271584, 0.45102681,
+                                     0.49493413, 0.53552665, 0.5735131 , 0.60938531, 0.64350111, 0.67613051,
+                                     0.70748321, 0.73772597, 0.76699401, 0.79539883, 0.82303369, 0.84997757,
+                                     0.87629806, 0.90205362, 0.92729522, 0.95206764, 0.97641053, 1.00035922,
+                                     1.02394538, 1.04719755, 1.07014161, 1.09280113, 1.11519765, 1.13735101,
+                                     1.15927948};
+
+
+    std::vector<double> zeni_smears= {0.20033484232311688, 0.0834592668852091, 0.06437191206463344, 0.05454982030770095, 0.04831097021560099, 0.0439073145446332, 0.04059252797349239, 0.03798645010870905, 0.03587220360769827, 0.034115800762489434, 0.03262940076737708, 0.031352702218681694, 0.030242756673905857, 0.029268039408617774, 0.028404822322277035, 0.027634861950832534, 0.026943873789830985, 0.026320495243533637, 0.02575556242418431, 0.025241594409087353, 0.02477241812103337, 0.024342890671188888, 0.023948690601140243, 0.023586158703977844, 0.023252175097645322, 0.022944063193710784, 0.022659513885635807, 0.02239652512312884, 0.022153353325937486, 0.021928474002397946};
+
     
-    double smear_pct{0.02}; // Percentage to smear the azimuth and zenith values by
-
-    // Create random number generator to smear bins by a small amount in azimuth and zenith
-    std::default_random_engine gen_gauss_azi;
-    gen_gauss_azi.seed(time(0));
-    std::normal_distribution<double> dist_azi(1.0,0.02);
-
-    std::default_random_engine gen_gauss_zeni;
-    gen_gauss_zeni.seed(time(0));
-    std::normal_distribution<double> dist_zeni(1.0,0.02);
+    double smear_pct_z{0.00}; // Percentage to smear the zenith values by
+    double smear_pct_a{0.00}; // Percentage to smear the azimuth values by
 
     TVector3 dir;
 
+    // Index to set a unique seed to the generator
+    int index = 0;
+
     for (auto & ran_idx: samples){
-        
+
         // Generate random index weighted by the bin contents
         ran_idx = dist(gen);
+
+        // Loop over the zenith values and find the appropriate smear value
+        for (int i = 0; i < zeni_edges.size()-1; i++){
+            if (zenith.at(ran_idx) >= zeni_edges.at(i) && zenith.at(ran_idx) < zeni_edges.at(i+1)){
+                smear_pct_z = zeni_smears.at(i);
+            }
+        }
+
+        // Azimuth has appropriate smear values
+        smear_pct_a = 0.10833078;
+
+        // Create random number generator to smear bins by a small amount in azimuth and zenith
+        std::default_random_engine gen_gauss_azi;
+        gen_gauss_azi.seed(time(0)*index);
+        std::normal_distribution<double> dist_azi(0, smear_pct_a);
+
+        std::default_random_engine gen_gauss_zeni;
+        gen_gauss_zeni.seed(time(0)*index+100); // Extra factor 100 to keep the seeds unique
+        std::normal_distribution<double> dist_zeni(0, smear_pct_z);
         
         // Get the Gaussian smear values
         double smear_azi  = dist_azi(gen_gauss_azi);
         double smear_zeni = dist_zeni(gen_gauss_zeni);
+
+        // if (zenith.at(ran_idx) == 0)
+        //     std::cout << smear_zeni << std::endl;
+
+        double a_ = azimuth.at(ran_idx)+smear_azi;
+        double z_ = zenith.at(ran_idx)+smear_zeni;
         
         // Fill histogram
-        hist->Fill(azimuth.at(ran_idx)*smear_azi, zenith.at(ran_idx)*smear_zeni);
-
-        double a_ = azimuth.at(ran_idx)*smear_azi;
-        double z_ = zenith.at(ran_idx)*smear_zeni;
+        hist->Fill(a_, z_);
 
         dir.SetX(sin(z_) * sin(a_));
         dir.SetY(-cos(z_));
@@ -51,6 +80,7 @@ void GenerateRandom(std::vector<double> weights, std::vector<double> azimuth, st
         histXZ->Fill(dir.X(), dir.Z());
         histYZ->Fill(dir.Y(), dir.Z());
 
+        index++;
 
     }
 
@@ -77,11 +107,11 @@ void RandomGenerator(){
 
     // Rotate the azimuth and zenith
     TRotation *rPhi = new TRotation();
-    rPhi->RotateY(-150);
+    rPhi->RotateY(-140);
     
     TVector3 dir;
 
-    for (int i = 0; i < 1000000; i++){
+    for (int i = 0; i < 1e7; i++){
         double zenith  = 0.;
         double azimuth = 0.;
         hist->GetRandom2(azimuth, zenith);
@@ -143,13 +173,28 @@ void RandomGenerator(){
 
 
     // Histogram for testing if random number generator is working
-    TH2D* hist_cpp2   = new TH2D("hist_cpp2", ";Azimuth; Zenith", 50, 0, 2.0*3.141 ,50, 0, 0.5*3.141);
-    TH2D* histXY_cpp2 = new TH2D("histXY_cpp2", ";X; Y", 50, -1, 1 , 25, -1, 0 );
-    TH2D* histXZ_cpp2 = new TH2D("histXZ_cpp2", ";X; Z", 50, -1, 1 , 50, -1, 1 );
-    TH2D* histYZ_cpp2 = new TH2D("histYZ_cpp2", ";Y; Z", 25, -1, 0 , 50, -1, 1 );
+    double binEdges_azi[] = {0.        , 0.21666156, 0.43332312, 0.64998469, 0.86664625, 1.08330781,
+                             1.29996937, 1.51663094, 1.7332925 , 1.94995406, 2.16661562, 2.38327719,
+                             2.59993875, 2.81660031, 3.03326187, 3.24992343, 3.466585  , 3.68324656,
+                             3.89990812, 4.11656968, 4.33323125, 4.54989281, 4.76655437, 4.98321593,
+                             5.1998775 , 5.41653906, 5.63320062, 5.84986218, 6.06652374, 6.28318531 };
+
+    double binEdges_zeni[] = {0.        , 0.20033484, 0.28379411, 0.34816602, 0.40271584, 0.45102681,
+                              0.49493413, 0.53552665, 0.5735131 , 0.60938531, 0.64350111, 0.67613051,
+                              0.70748321, 0.73772597, 0.76699401, 0.79539883, 0.82303369, 0.84997757,
+                              0.87629806, 0.90205362, 0.92729522, 0.95206764, 0.97641053, 1.00035922,
+                              1.02394538, 1.04719755, 1.07014161, 1.09280113, 1.11519765, 1.13735101,
+                              1.15927948};
+
+
+    TH2D* hist_cpp2   = new TH2D("hist_cpp2", ";Azimuth; Zenith", 29, binEdges_azi , 30, binEdges_zeni);
+    TH2D* histXY_cpp2 = new TH2D("histXY_cpp2", ";X; Y", 100, -1, 1 , 50, -1, 0 );
+    TH2D* histXZ_cpp2 = new TH2D("histXZ_cpp2", ";X; Z", 100, -1, 1 , 100, -1, 1 );
+    TH2D* histYZ_cpp2 = new TH2D("histYZ_cpp2", ";Y; Z", 50, -1, 0 , 100, -1, 1 );
 
     // File pointer
-    std::ifstream fin("MeasuredMuonsFromData.csv");
+    // std::ifstream fin("MeasuredMuonsFromData.csv");
+    std::ifstream fin("SimulatedMuonsFromProposal.csv");
     
     // Check if file has opened properly
     if (!fin.is_open())
@@ -185,8 +230,6 @@ void RandomGenerator(){
 
     TCanvas *cYZ_cpp2 = new TCanvas();
     histYZ_cpp2->Draw("colz");
-
-
 
 
 }
