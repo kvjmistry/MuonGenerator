89318a3f5f68494b5cd7909c35cc7df586ffac56
Krishan Mistry <kvjmistry@gmail.com>
2022-03-01 12:23:09 -0600

    fixing slim file file input

--- slim_file.py
+++ slim_file.py
@@ -6,10 +6,12 @@ from invisible_cities.io.mcinfo_io import load_mcparticles_df
 filewildcard = sys.argv[1]
 files = glob.glob(filewildcard)
 
-print(files)
+print(filewildcard)
 
 
-data = load_mcparticles_df(files) 
+data = load_mcparticles_df(filewildcard) 
+
+print(data)
 
 file_out = sys.argv[2]
 pd.DataFrame(data).to_hdf(file_out,'Data')
